de236549b6781d5ebab10daf07c5f9ab5dced25e
Eugenia Guerrero <angeleuge95@gmail.com>
2017-10-09 13:29:12 +0100

    Working program for question 4.a

--- ExerciseSheet5/Question4/main.cpp
+++ ExerciseSheet5/Question4/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 enum Day {Sun, Mon, Tue, Wed, Thu, Fri, Sat};
 
 int closing_time(Day day_of_the_week);
-int print_day(Day day_of_the_week);
+void print_day(Day _day, ostream &out_stream);
 
 int main()
 {
@@ -19,23 +19,23 @@ int main()
     {
         cout.width(19);
 //        switch (count)
-        cout << print_day(static_cast<Day>(count), cout);
+        print_day(static_cast<Day>(count), cout);
         cout << closing_time(static_cast<Day>(count)) << "pm\n";
     }
     return 0;
 }
 
-int print_day(Day day_of_the_week)
+void print_day(Day _day, ostream &out_stream)
 {
-        switch (count)
+        switch (_day)
             {
-            case Sun: cout << "Sunday"; break;
-            case Mon: cout << "Monday"; break;
-            case Tue: cout << "Tuesday"; break;
-            case Wed: cout << "Wednesday"; break;
-            case Thu: cout << "Thursday"; break;
-            case Fri: cout << "Friday"; break;
-            case Sat: cout << "Saturday"; break;
+            case Sun: out_stream << "Sunday"; break;
+            case Mon: out_stream << "Monday"; break;
+            case Tue: out_stream << "Tuesday"; break;
+            case Wed: out_stream << "Wednesday"; break;
+            case Thu: out_stream << "Thursday"; break;
+            case Fri: out_stream << "Friday"; break;
+            case Sat: out_stream << "Saturday"; break;
             }
 }
 
